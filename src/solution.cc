26465ed30a6c21df0b449c49ea4e202b8215b00b
kmbruss <kaanbruss@gmail.com>
2026-02-09 13:47:32 -0600

    Refactor ReadStudentRecFromStream for better readability

--- src/solution.cc
+++ src/solution.cc
@@ -2,15 +2,11 @@
 
 Student ReadStudentRecFromStream(std::istream& is) {
   // your implementation here...
-  while (is.good()) {
-    std::string name = "";
-    unsigned int uin = 0;
-    double gpa = 0.0;
-    char throw_away = '\0';
-    is >> name >> throw_away >> uin >> throw_away >> gpa;
-    if (is.fail()) {
-      break;
-    }
+  std::string name = "";
+  unsigned int uin = 0;
+  double gpa = 0.0;
+  char throw_away = '\0';
+  if (is >> name >> throw_away >> uin >> throw_away >> gpa;) {
     return Student(name, uin, gpa);
   }
   return Student{};
