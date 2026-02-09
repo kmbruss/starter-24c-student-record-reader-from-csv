2ff0325a4a1c44964f781447797c55dc1a5121ce
kmbruss <kaanbruss@gmail.com>
2026-02-09 13:32:29 -0600

    Add ReadStudentRecFromStream function implementation
    Implement reading student records from input stream.

--- src/solution.cc
+++ src/solution.cc
@@ -2,5 +2,16 @@
 
 Student ReadStudentRecFromStream(std::istream& is) {
   // your implementation here...
+  while (is.good) {
+    std::string name = "";
+    unsigned int uin = 0;
+    double gpa = 0.0;
+    char throw_away = '\0';
+    is >> name >> throw_away >> uin >> throw_away >> gpa;
+    if (is.fail()) {
+      break;
+    }
+    return student(name, uin, gpa);
+  }
   return Student{};
 }
