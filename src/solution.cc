78caa507b38eaae8e5c78409c76b0c2dd30cd6b0
kmbruss <kaanbruss@gmail.com>
2026-02-09 14:22:55 -0600

    Fix GPA initialization and lastname loop condition
    Fixed syntax error by adding a semicolon after GPA initialization and adjusted loop condition for lastname processing.

--- src/solution.cc
+++ src/solution.cc
@@ -5,7 +5,7 @@ Student ReadStudentRecFromStream(std::istream& is) {
   std::string firstname = "";
   std::string lastname = "";
   unsigned int uin = 0;
-  double gpa = 0.0
+  double gpa = 0.0;
   char throw_away = '\0';
   
   if (!(is >> firstname >> lastname >> uin >> throw_away >> gpa)) {
@@ -20,7 +20,7 @@ Student ReadStudentRecFromStream(std::istream& is) {
   }
   
   std::string last = "";
-  for (int i = 0; i < lastname.length() - 2; i++) {
+  for (int i = 0; i < lastname.length() - 1; i++) {
       last += lastname[i];
   }
   return Student(firstname + " " + last, uin, gpa);
