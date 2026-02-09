adf6c9b8cd5c2289092fe9ada8a6c3c931a44e7d
kmbruss <kaanbruss@gmail.com>
2026-02-09 14:05:22 -0600

    Update ReadStudentRecFromStream implementation
    Refactor ReadStudentRecFromStream to read first and last names.

--- src/solution.cc
+++ src/solution.cc
@@ -2,10 +2,21 @@
 
 Student ReadStudentRecFromStream(std::istream& is) {
   // your implementation here...
-  std::string name = "";
+  std::string firstname = "";
+  std::string lastname = "";
   unsigned int uin = 0;
   double gpa = 0.0;
-  char throw_away = '\0';
-  is >> name >> throw_away >> uin >> throw_away >> gpa;
-  return Student(name, uin, gpa);
+  char firstThrow = '\0';
+  char secondThrow = '\0';
+  
+  if (is >> firstname >> lastname >> firstThrow >> uin >> secondThrow >> gpa) {
+    if (firstThrow == ',' && secondThrow == ',') {
+      return Student{};
+    }
+    return student(firstname + " " + lastname, uin, gpa);
+  }
+  // if (is.fail()) {
+  //   return Student{};
+  // }
+  return Student{};
 }
