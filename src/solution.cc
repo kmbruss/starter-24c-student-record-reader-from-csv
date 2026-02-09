5b0ae4ba9033b7c3a82f33f5e9710a4767e0e074
kmbruss <kaanbruss@gmail.com>
2026-02-09 13:48:11 -0600

    Fix input stream extraction syntax in solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -6,7 +6,7 @@ Student ReadStudentRecFromStream(std::istream& is) {
   unsigned int uin = 0;
   double gpa = 0.0;
   char throw_away = '\0';
-  if (is >> name >> throw_away >> uin >> throw_away >> gpa;) {
+  if (is >> name >> throw_away >> uin >> throw_away >> gpa) {
     return Student(name, uin, gpa);
   }
   return Student{};
